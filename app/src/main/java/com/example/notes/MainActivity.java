4991f8500b20b816ba88202880f4ea42db66a08c
moumita <mdas.160395@gmail.com>
2020-06-02 13:20:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.notes;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    static ArrayList<String> notes= new ArrayList<String>();
+    static  ArrayAdapter arrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView=findViewById(R.id.listView);
+        notes.add("Example note");
+            arrayAdapter =new ArrayAdapter(this, android.R.layout.simple_list_item_1,notes);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+                Intent intent=new Intent(getApplicationContext(),NoteEditorActivity.class);
+                intent.putExtra("noteID",i);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+    //MENU
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater =getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+            if(item.getItemId()==R.id.add_note)
+
+
+    }
+
+
+}
